4124d18594d36e3dd3ffb320588c81d7d68b71b5
opoliakova <olena.poliakova@timecode.co>
2016-11-02 19:12:46 +0200

    AccountsController: added method ValidateToken, switched to use new methods in GeneratingJWT

--- JSONWebToken/Controllers/AccountsController.cs
+++ JSONWebToken/Controllers/AccountsController.cs
@@ -7,26 +7,53 @@ using System.Web.Http;
 using Microsoft.IdentityModel.Tokens;
 using System.IdentityModel.Tokens.Jwt;
 using JSONWebToken.Models;
+using JSONWebToken.Signature;
+using System.Text;
 
 namespace JSONWebToken.Controllers
 {
     [RoutePrefix("api/account")]
     public class AccountsController : ApiController
     {
+
+        private JwtSecurityToken ValidateToken(JwtSecurityTokenHandler tokenHandler, string jwt)
+        {
+            SecurityToken token;
+            var signingCredentials = new SymmetricSecurityKey(KeyGeneration.hmac.Key);
+            TokenValidationParameters validationParams = new TokenValidationParameters
+            {
+                RequireExpirationTime = true,
+                ValidateLifetime = true,
+
+                RequireSignedTokens = true,
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = signingCredentials,
+                CryptoProviderFactory = new CryptoProviderFactory(),
+
+                ValidateActor = false,
+                ValidateAudience = false,
+                ValidateIssuer = false,
+            };
+            tokenHandler.ValidateToken(jwt, validationParams, out token);
+            return (JwtSecurityToken)token;
+        }
+
         [Route("")]
         public IHttpActionResult Get()
         {
             string encodedJWT = Request.Headers.GetValues("JWT").First();
 
             JwtSecurityTokenHandler tokenHandler = new JwtSecurityTokenHandler();
+            //ValidateToken(tokenHandler, encodedJWT);
             JwtSecurityToken decodedJWT;
             try
             {
-                decodedJWT = tokenHandler.ReadJwtToken(encodedJWT);
+                decodedJWT = ValidateToken(tokenHandler, encodedJWT);
+                //decodedJWT = tokenHandler.ReadJwtToken(encodedJWT);
             }
-            catch (Exception)
+            catch (Exception e)
             {
-                return BadRequest("token was not correct");
+                return BadRequest(e.Message);
             }
 
             return Ok(decodedJWT);
@@ -36,7 +63,8 @@ namespace JSONWebToken.Controllers
         public IHttpActionResult GetToken()
         {
             string name = Request.Headers.GetValues("UserName").First();
-            string token = (new GeneratingJWT()).GenerateJWT(new User { UserName = name, App = "app", Device = "device" });
+            //string token = (new GeneratingJWT()).GenerateJSON(new User { UserName = name, App = "app", Device = "device" });
+            string token = (new GeneratingJWT()).GenerateJWTWithSignature(new User { UserName = name, App = "app", Device = "device" });
             return Ok(token);
         }
 
@@ -52,5 +80,6 @@ namespace JSONWebToken.Controllers
         {
             return Ok("created " + createUserModel.UserName);
         }
+
     }
 }
