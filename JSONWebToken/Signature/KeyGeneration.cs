9cc1c80d6167116b9e1da11548ca4f4f65173e41
opoliakova <olena.poliakova@timecode.co>
2016-11-02 19:06:31 +0200

    KeyGeneration: Deleted unused methods

--- JSONWebToken/Signature/KeyGeneration.cs
+++ JSONWebToken/Signature/KeyGeneration.cs
@@ -4,23 +4,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Security.Cryptography;
 using System.Web;
-using Thinktecture.IdentityModel.Tokens;
 
 namespace JSONWebToken.Signature
 {
     public static class KeyGeneration
     {
         public static readonly HMACSHA256 hmac = new HMACSHA256();
-        
-        public static string GetBase64Key()
-        {
-            return Convert.ToBase64String(hmac.Key);
-        }
-
-        public static HmacSigningCredentials GetSigningCredentials()
-        {
-            return new HmacSigningCredentials(hmac.Key);
-        }
 
         public static string GetAlgorithm()
         {
